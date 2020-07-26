9d1aa14a41bc059d1e0cd26b12d27fc9e436a9e8
b2blosangeles <b2blosangeles@gmail.com>
2020-07-25 21:49:55 -0700

    Update run_nginx_proxy.sh

--- nginx_proxy/run_nginx_proxy.sh
+++ nginx_proxy/run_nginx_proxy.sh
@@ -1,3 +1,19 @@
-DIR=$( cd ${0%/*} && pwd -P )
-echo "===66==>>"
-echo $DIR
+myDir=$( cd ${0%/*} && pwd -P )
+docker network rm network_ui_app &> /dev/null
+
+docker network create \
+    --driver=bridge \
+    --subnet=10.10.10.0/16 \
+    --ip-range=10.10.10.0/24 \
+    --gateway=10.10.10.254 \
+    network_ui_app &> /dev/null
+        
+docker build -t some-content-nginx .
+docker stop some-nginx
+docker rm some-nginx
+
+docker stop local_proxy_container
+docker rm local_proxy_container
+
+
+docker run --name some-nginx --network network_ui_app -v "$(pwd)/html":/usr/share/nginx/html -p 80:80 -d some-content-nginx
