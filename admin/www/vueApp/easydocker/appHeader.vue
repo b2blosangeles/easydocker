7add7bf05b5e89ad0fde8ee4929dff3eef631e7f
b2blosangeles <b2blosangeles@gmail.com>
2020-07-26 09:04:21 -0700

    Update appHeader.vue

--- admin/www/vueApp/easydocker/appHeader.vue
+++ admin/www/vueApp/easydocker/appHeader.vue
@@ -3,9 +3,12 @@
         <div class="card-body alert-warning border border-warning">
             <div class="container-fluid m-0">
                 <div class="row">
-                    <div class="col-1 p-0 m-0 text-right">
+                    <div class="col-2 p-0 m-0 text-left">
+                        <a class="btn btn-sm btn-success m-1 pull-right" href="JavaScript:void(0)" v-on:click="restartProxy()">
+                                Restart proxy
+                        </a>
                     </div>
-                    <div class="col-9 p-0 m-0 text-center">
+                    <div class="col-8 p-0 m-0 text-center">
                             <h4>EasyDocker Admin.</h4>
                     </div>
                     <div class="col-2 p-0 m-0 text-right">
@@ -32,6 +35,11 @@ module.exports = {
         addVHost() {
             var me = this;
             me.$parent.module = (me.$parent.module === 'form') ? 'list' : 'form';
+        },
+        restartProxy() {
+            var me = this;
+            alert('restartProxy()');
+            // me.$parent.module = (me.$parent.module === 'form') ? 'list' : 'form';
         }
     }
 }
