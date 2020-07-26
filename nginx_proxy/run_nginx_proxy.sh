b08db43079dd699fc0f7d0006fe834c6d7415428
b2blosangeles <b2blosangeles@gmail.com>
2020-07-25 23:42:09 -0700

    Update run_nginx_proxy.sh

--- nginx_proxy/run_nginx_proxy.sh
+++ nginx_proxy/run_nginx_proxy.sh
@@ -12,4 +12,4 @@ docker rm nginx-proxy-container
 docker stop nginx-proxy-container
 docker rm nginx-proxy-container
 
-docker run --name nginx-proxy-container --network network_ui_app -v "$myDir/html":/usr/share/nginx/html -p 80:80 -d nginx-proxy-image
+docker run --name nginx-proxy-container --network network_ui_app -v "$myDir/html":/usr/share/nginx/html -v "$DATA_DIR"/proxy:/usr/share/nginx/proxy_config -p 80:80 -d nginx-proxy-image
