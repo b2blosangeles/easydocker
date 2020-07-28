e69d231b495e9d9aaa0b3c70c4a42dad12cfc127
b2blosangeles <b2blosangeles@gmail.com>
2020-07-27 23:55:20 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,47 +1,6 @@
 #!/bin/bash
 git pull
 
-# ---- prepare S -----
-DOCKERCMD=$(command -v docker)
-OSENV=""
-case "$(uname -s)" in
-
-   Darwin)
-     OSENV='Mac'
-     ;;
-   Linux)
-     OSENV='Linux'
-     ;;
-
-   CYGWIN*|MINGW32*|MSYS*|MINGW*)
-     OSENV='MS Windows'
-     ;;
-   *)
-     OSENV='' 
-     ;;
-esac
-
-if [ $DOCKERCMD = '' ]; then
-    echo "Error : Docker installation and running is required!"
-    exit 0
-fi
-
-if [ $OSENV != "Mac" ]; then
-    echo "this is $OSENV environment."
-    echo "Error : We only support Mac OS X now!"
-    exit 0
-fi
-
-if [ $USER = "root" ] ;
-then
-   echo "Running as sudo ..."
-else
-   echo "Error : Need root user to run the command!"
-   exit 0
-fi
-# ---- prepare E ---
-
-
 SCR_DIR=$(cd `dirname $0` && pwd)
 SCRIPTFN=$(basename -- $SCR_DIR)
 DATA_DIR="$(dirname "$SCR_DIR")/_"$SCRIPTFN"_DATA"
