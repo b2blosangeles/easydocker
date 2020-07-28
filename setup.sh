02ad29734b8dee950d984fda513f87e716ca2820
b2blosangeles <b2blosangeles@gmail.com>
2020-07-27 23:47:47 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -45,10 +45,11 @@ fi
 echo "setup cronjob=$OSENV"
 
 if [ $OSENV = "Linux" ]; then
+   SCRIPTDIR=$(cd `dirname $0` && pwd)
    echo "Running on Linux ..."
    sed '/echo _EASYDOCKER/d' /etc/crontab  > /tmp/crontab_easydocker
    cp -f /tmp/crontab_easydocker  /etc/crontab
-   echo "@reboot echo _EASYDOCKER && sh start.sh" >> /etc/crontab
+   echo "@reboot echo _EASYDOCKER && cd $SCRIPTDIR && sh start.sh" >> /etc/crontab
 fi
 # ---- setup cronjob and file permission E ---
 
