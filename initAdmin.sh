efe4e56473b4c6885e6d2fd38009220f38c6bbfd
b2blosangeles <b2blosangeles@gmail.com>
2020-07-28 17:24:59 -0700

    Update initAdmin.sh

--- initAdmin.sh
+++ initAdmin.sh
@@ -1,8 +1,15 @@
 SCR_DIR=$(cd `dirname $0` && pwd)
 SCRIPTFN=$(basename -- $SCR_DIR)
 DATA_DIR="$(dirname "$SCR_DIR")/_"$SCRIPTFN"_DATA"
+
+
+dockerDir="${SCR_DIR}/dockerFiles/admin_dockerfile/"
+
+
 echo $SCR_DIR
 echo $DATA_DIR
+echo $dockerDir
+
 << 'MULTILINE-COMMENT'
 
 const process = require('process');
