5b589374e613140afb0d97234267463b801f2500
b2blosangeles <b2blosangeles@gmail.com>
2020-07-29 08:46:36 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -63,17 +63,10 @@ if [ $OSENV = "Linux" ]; then
     #for (( i=1; i < 60; i+=1 ))
     #for i in {1..60..1}
     COMM="sh ${SCR_DIR}/cron.sh >> ${DATA_DIR}/log/crontask_$SUDO_USER.log"
-    for i in {1..60}
+    for i in $(seq 0.5 0.5 59.5)
     do
       echo "* * * * *  (sleep $i ; echo _EASY_DOCKER && $COMM)" >> /etc/crontab
     done
-   
-      for i in $(seq 0.5 0.5 59.5)
-      do
-         echo "Welcome $i times"
-      done
-
-
 fi
 # ---- setup cronjob and file permission E ---
 
