0b2f8d0c181f72a43c6517fd6f778848cb7a5ce1
b2blosangeles <b2blosangeles@gmail.com>
2020-07-29 20:11:32 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -82,7 +82,7 @@ fi
 
 # ----- nginx proxy code Start  -----#
 sleep 1;
-sh ./nginx_proxy/run_nginx_proxy.sh $DATA_DIR > /var/ccc.log
+sh ${SCR_DIR}/nginx_proxy/run_nginx_proxy.sh $DATA_DIR > /var/ccc.log
 # ----- nginx proxy code End  -----#
 
 if [ "$OSENV" = "Linux" ]; then
