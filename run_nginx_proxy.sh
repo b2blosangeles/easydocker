405d7661f963e6d3d6985a6bc71664455659ddbe
b2blosangeles <b2blosangeles@gmail.com>
2020-07-29 21:07:29 -0700

    Update run_nginx_proxy.sh

--- run_nginx_proxy.sh
+++ run_nginx_proxy.sh
@@ -9,7 +9,7 @@ CMD="${CMD}docker build -t nginx-proxy-image  .\n";
 CMD="${CMD}docker container stop nginx-proxy-container\n";
 CMD="${CMD}docker container rm nginx-proxy-container\n";
 CMD="${CMD}docker run -d -p 80:80 -v \"${SCR_DIR}/nginx_proxy/html\":/usr/share/nginx/html -v \"${DATA_DIR}/proxy\":/usr/share/nginx/proxy_config ";
-CMD="${CMD} --network network_ui_app  ";
+# CMD="${CMD} --network network_ui_app  ";
 CMD="${CMD} --name nginx-proxy-container nginx-proxy-image\n";
 
 shell_proxy="${DATA_DIR}/_cron/proxy_$(date +%s%N).sh"
