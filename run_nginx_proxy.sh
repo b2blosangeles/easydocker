9256ed228550a60a0e85930e3823d2aad0fab867
b2blosangeles <b2blosangeles@gmail.com>
2020-07-29 20:59:41 -0700

    Update and rename nginx_proxy/run_nginx_proxy.sh to run_nginx_proxy.sh

--- run_nginx_proxy.sh
+++ run_nginx_proxy.sh
@@ -1,13 +1,14 @@
-myDir=$( cd ${0%/*} && pwd -P )
-DATA_DIR="$1"
+SCR_DIR=$(cd `dirname $0` && pwd)
+SCRIPTFN=$(basename -- $SCR_DIR)
+DATA_DIR="$(dirname "$SCR_DIR")/_"$SCRIPTFN"_DATA"
 
 CMD=""
 CMD="${CMD}Start proxy ..\n";
-CMD="${CMD}cd ${myDir}\n";
+CMD="${CMD}cd ${SCR_DIR}/nginx_proxy\n";
 CMD="${CMD}docker build -t nginx-proxy-image  .\n";
 CMD="${CMD}docker container stop nginx-proxy-container\n";
 CMD="${CMD}docker container rm nginx-proxy-container\n";
-CMD="${CMD}docker run -d -p 80:80 -v \"${myDir}/html\":/usr/share/nginx/html -v \"${DATA_DIR}/proxy\":/usr/share/nginx/proxy_config ";
+CMD="${CMD}docker run -d -p 80:80 -v \"${SCR_DIR}/nginx_proxy/html\":/usr/share/nginx/html -v \"${DATA_DIR}/proxy\":/usr/share/nginx/proxy_config ";
 CMD="${CMD} --network network_ui_app  ";
 CMD="${CMD} --name nginx-proxy-container nginx-proxy-image\n";
 
