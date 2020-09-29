0274ccb7a1ebfccaa3e841850272560a86163cea
b2blosangeles <b2blosangeles@gmail.com>
2020-09-29 06:56:43 -0700

    update

--- admin/www/vueApp/easydocker/popUpModals/switchBranch.vue
+++ admin/www/vueApp/easydocker/popUpModals/switchBranch.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>switchBranch => {{this.$parent.cfg.data.serverName}}</div>
+    <div>
+        switchBranch => {{this.$parent.cfg.data.serverName}} - {{this.$parent.cfg.data.branch}}
+        {{branches}}
+    </div>
 </template>
 
 <script>
@@ -19,14 +22,10 @@ module.exports = {
         gitRemoteBranchs(serverName) {
             var me = this;
             me.root.dataEngine().gitSiteBranchs(serverName, function(result) {
-                console.log(result);
-                return true;
-
                 if (result.status === 'success') {
-                    me.branches = result.list;
+                    me.branches = result.list.branches;
                 } else {
                     me.branches = [];
-                    me.errors.gitHub = result.message;
                 }
             });
         },
